f7ea15ea29cff5f8b90f31d85185f7ecef80b950
jfro1978 <39474907+jfro1978@users.noreply.github.com>
2019-07-24 20:42:56 +1000

    Update quad_tx.ino

--- quad_tx.ino
+++ quad_tx.ino
@@ -10,6 +10,9 @@
 
 #include <SPI.h>
 #include <RF24.h>
+
+
+
 int packet[1];
 RF24 radio(7,8); // CE and CSN pins respectively on the Arduino
 
@@ -23,7 +26,6 @@ void setup() {
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_250KBPS);
   radio.stopListening();
-
 }
 
 void loop() {
